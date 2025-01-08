e009f7ac5ecfc056a190731d7579433cd4e8e178
Saswata Biswas <saswata.biswas20@gmail.com>
2025-01-08 22:54:11 +0530

    init

--- ui/ui.py
+++ ui/ui.py
@@ -1,1 +1,26 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Label, Header, Footer, Static
+from textual.containers import Container
 
+class EthosMusicCLI(App):
+
+    CSS_PATH = "./ui.tcss"
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        yield Container(
+            Static("ethos", classes="title"),
+            Static("music cli for cool folks", classes="sub_title"),
+            id="hero"
+        )
+
+    def on_mount(self) -> None:
+        self.title = "ethos"
+        self.sub_title = "music cli"
+        self.screen.styles.background = "black"
+        self.screen.styles.border = ("heavy", "green")
+
+if __name__ == "__main__":
+    app = EthosMusicCLI()
+    app.run()
