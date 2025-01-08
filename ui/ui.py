42f4411c6186124fd4393e948c4f7f5af6857764
Saswata Biswas <saswata.biswas20@gmail.com>
2025-01-09 01:20:43 +0530

    config hero section

--- ui/ui.py
+++ ui/ui.py
@@ -6,14 +6,36 @@ class EthosMusicCLI(App):
 
     CSS_PATH = "./ui.tcss"
 
+    
+
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
+
+        s = '''   ▄████████     ███        ▄█    █▄     ▄██████▄     ▄████████ 
+  ███    ███ ▀█████████▄   ███    ███   ███    ███   ███    ███ 
+  ███    █▀     ▀███▀▀██   ███    ███   ███    ███   ███    █▀  
+ ▄███▄▄▄         ███   ▀  ▄███▄▄▄▄███▄▄ ███    ███   ███        
+▀▀███▀▀▀         ███     ▀▀███▀▀▀▀███▀  ███    ███ ▀███████████ 
+  ███    █▄      ███       ███    ███   ███    ███          ███ 
+  ███    ███     ███       ███    ███   ███    ███    ▄█    ███ 
+  ██████████    ▄████▀     ███    █▀     ▀██████▀   ▄████████▀  
+                                                                '''
+        
+        subtitle = '''               _         _ _    __                       _    __     _ _       
+  _ __ _  _ __(_)__   __| (_)  / _|___ _ _   __ ___  ___| |  / _|___| | |__ ___
+ | '  \ || (_-< / _| / _| | | |  _/ _ \ '_| / _/ _ \/ _ \ | |  _/ _ \ | / /(_-<
+ |_|_|_\_,_/__/_\__| \__|_|_| |_| \___/_|   \__\___/\___/_| |_| \___/_|_\_\/__/
+                                                                               '''
+        
+        #hero-section
         yield Container(
-            Static("ethos", classes="title"),
-            Static("music cli for cool folks", classes="sub_title"),
+            Static(s, classes="title"),
+            Static(subtitle, classes="sub_title"),
             id="hero"
         )
+        #neko-section
+
 
     def on_mount(self) -> None:
         self.title = "ethos"
